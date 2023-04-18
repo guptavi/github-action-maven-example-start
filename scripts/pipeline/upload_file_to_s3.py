8a1124632a822889ca0c8721c65a46a3dbf55614
BharatEncora <95352198+BharatEncora@users.noreply.github.com>
2023-04-18 17:21:12 +0530

    Update upload_file_to_s3.py

--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -1,18 +1,25 @@
 import boto3
 import sys
 
+
 def main():
-    if (len(sys.argv) == 6):
-        print ('Error: Required 5 arguments.')
+    if len(sys.argv) == 6:
+        print('Error: Required 5 arguments.')
         # Checks for 6 because the script path is in position 0. So len is 6
         # for 5 arguments.
         sys.exit(1)
+    print(sys.argv[1]);
+    print(sys.argv[2]);
+    print(sys.argv[3]);
+    print(sys.argv[4]);
+    print(sys.argv[5]);
+    
 
-    bucket_name=sys.argv[1]
-    aws_key=sys.argv[2]
-    aws_access_key=sys.argv[3]
-    aws_access_secret=sys.argv[4]
-    local_path=sys.argv[5]
+    bucket_name = sys.argv[1]
+    aws_key = sys.argv[2]
+    aws_access_key = sys.argv[3]
+    aws_access_secret = sys.argv[4]
+    local_path = sys.argv[5]
 
     session = boto3.Session(
         aws_access_key_id=aws_access_key,
@@ -25,7 +32,7 @@ def main():
         Bucket=bucket_name,
         Key=aws_key
     )
-    print ('Done uploading')
+    print('Done uploading')
 
 
 main()
